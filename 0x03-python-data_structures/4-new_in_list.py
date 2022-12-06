bd0dcb68754bf7a311d2e9c69af779ab116ca98c
Ade-ojo Tolulope <tolexskii@gmail.com>
2022-12-06 09:29:38 -0800

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,10 @@ def new_in_list(my_list, idx, element):
 
     new_list = my_list[:]
 
-    if 0 <= idx < length:
+    if idx < 0:
+        return (new_list)
+    elif idx >= length:
+        return (new_list)
+    else:
         new_list[idx] = element
-
         return (new_list)
