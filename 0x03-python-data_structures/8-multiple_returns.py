5296ad625ac4bfa7ed48e1f079e0915ea0bb9866
Ade-ojo Tolulope <tolexskii@gmail.com>
2022-12-06 11:11:21 -0800

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,6 @@
 def multiple_returns(sentence):
     len_s = len(sentence)
 
-    if (len_s == 0):
-        new_tuple = len_s, None)
-    else:
-        new_tuple = (len_s, sentence[0])
-
-    return (new_tuple)
+    if len_s == 0:
+        return (len_s, None)
+    return (len_s, sentence[0])
